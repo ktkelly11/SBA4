87353c6ac044941c813945374bdefe80ddd4b8a4
Katherine Kelly <ktkelly11@gmail.com>
2024-05-15 22:28:56 -0400

    more javascript

--- index.mjs
+++ index.mjs
@@ -3,3 +3,8 @@ console.log("First file");
 import "./script.mjs";
 import "./third.mjs";
 // can also use "import {name} from "file name""
+
+const breedSelect = document.getElementById("dog");
+
+const API_Key =
+  "live_gq7bJyh2oCEFvPNVektDFnCpic3JV1FRHN1wDIfEdcEv7TbqXS7WbcftT5VbrqCF";
