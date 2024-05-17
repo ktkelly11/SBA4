58161793ffadb16b383f45e0d7e8339503bad558
Katherine Kelly <ktkelly11@gmail.com>
2024-05-16 20:03:17 -0400

    Trying to fiddle with JS

--- index.mjs
+++ index.mjs
@@ -4,7 +4,18 @@ import "./script.mjs";
 import "./third.mjs";
 // can also use "import {name} from "file name""
 
-const breedSelect = document.getElementById("dog");
+const dropdown = document.getElementById("dog");
 
 const API_Key =
   "live_gq7bJyh2oCEFvPNVektDFnCpic3JV1FRHN1wDIfEdcEv7TbqXS7WbcftT5VbrqCF";
+
+const url = " https://api.thedogapi.com/v1";
+let storedBreeds = [];
+
+// fetch("https://api.thedogapi.com/v1/breeds")
+//   .then((res) => {
+//     return res.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   });
