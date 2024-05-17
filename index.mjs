ae01d971daf85294de28f29acd7f5dcaa25295fb
Katherine Kelly <ktkelly11@gmail.com>
2024-05-16 23:25:16 -0400

    got dropdown working

--- index.mjs
+++ index.mjs
@@ -9,35 +9,27 @@ const breedSelect = document.getElementById("dog");
 const API_Key =
   "live_gq7bJyh2oCEFvPNVektDFnCpic3JV1FRHN1wDIfEdcEv7TbqXS7WbcftT5VbrqCF";
 
-const url = " https://api.thedogapi.com/v1";
-let storedBreeds = [];
+const url = " https://api.thedogapi.com/v1/breeds";
+// let storedBreeds = [];
 
-// fetch("https://api.thedogapi.com/v1/breeds")
-//   .then((res) => {
-//     return res.json();
-//   })
-//   .then((data) => {
-//     console.log(data);
-//   });
+function populateDropdown(breeds) {
+  breeds.forEach((breed) => {
+    const option = document.createElement("option");
+    option.textContent = breed.name;
+    option.setAttribute("value", breed.name);
+    breedSelect.appendChild(option);
+  });
+}
 
-// function populateDropdown(breeds) {
-//   breeds.forEach((breed) => {
-//     const option = document.createElement("option");
-//     option.textContent = breed.name;
-//     option.setAttribute("value", breed.name);
-//     breedSelect.appendChild(option);
-//   });
-// }
+async function fetchBreeds() {
+  try {
+    const response = await fetch("https://api.thedogapi.com/v1/breeds");
+    const data = await response.json();
+    console.log(data);
+    populateDropdown(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
 
-// async function fetchBreeds() {
-//   try {
-//     const response = await fetch("https://api.thedogapi.com/v1/breeds");
-//     const data = await response.json();
-//     console.log(data);
-//     populateDropdown(data.results);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
-
-// fetchBreeds();
+fetchBreeds();
